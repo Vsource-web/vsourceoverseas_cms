faa8f98012bce774ae58007457582d6c142771a6
Vsource-company <sagreact@gmail.com>
2025-09-22 16:35:09 +0530

    added fintech landing page

---
+++ src/api/fintech-about-us/controllers/fintech-about-us.ts
@@ -0,0 +1,7 @@
+/**
+ * fintech-about-us controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::fintech-about-us.fintech-about-us');
---
+++ src/api/fintech-about-us/routes/fintech-about-us.ts
@@ -0,0 +1,7 @@
+/**
+ * fintech-about-us router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::fintech-about-us.fintech-about-us');
