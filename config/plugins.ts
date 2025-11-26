2392f85c5e71d6fa68626f3acb28e09ab0473bf7
Nithish <164472246+Nithish-FullStack-Dev@users.noreply.github.com>
2025-11-26 22:26:14 +0530

    Switch upload provider to local
    Changed the upload provider from Cloudinary to local and updated size limit.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,18 +1,14 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "cloudinary",
+      provider: 'local',
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+        sizeLimit: 200 * 1024 * 1024, 
       },
       actionOptions: {
         upload: {},
-        uploadStream: {},
         delete: {},
       },
-      sizeLimit: 250 * 1024 * 1024, // 50MB
     },
   },
 });
